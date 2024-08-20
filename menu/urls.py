256034239f36be0e64497a75440050b1b1559be8
Mikhail <155720387+blessedforyou@users.noreply.github.com>
2024-08-20 19:07:29 +0300

    Update urls.py

--- menu/urls.py
+++ menu/urls.py
@@ -4,7 +4,4 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    path('product/new/', views.ProductView.as_view(), name='product-new'),
-    path('service/support/', views.ServiceView.as_view(), name='service-support'),
-    path('info/project-info/', views.InfoView.as_view(), name='info'),
 ]
