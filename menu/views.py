e029e0e864c99d7c8099b25b64c8599f2fcf5cfe
Mikhail <mikhail.sharov.25@gmail.com>
2024-08-20 18:58:46 +0300

    Fix views

--- menu/views.py
+++ menu/views.py
@@ -4,14 +4,3 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "menu/index.html"
 
-
-class ProductView(TemplateView):
-    template_name = "product/product.html"
-
-
-class ServiceView(TemplateView):
-    template_name = 'service/service.html'
-
-
-class InfoView(TemplateView):
-    template_name = 'info/project-info.html'
